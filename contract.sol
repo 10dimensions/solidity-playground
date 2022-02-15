ba18583963bc258d3855b5d76541855feaafe222
10dimensions <38999489+10dimensions@users.noreply.github.com>
2022-02-15 06:45:37 +0000

    Add English Auction contract

--- contract.sol
+++ contract.sol
@@ -1,5 +1,7 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.2;
+pragma solidity ^0.8.8;
+
+import "./contracts/auctions/dutchAuction.sol";
 
 contract SimpleStorage {
   uint256 storedData;
